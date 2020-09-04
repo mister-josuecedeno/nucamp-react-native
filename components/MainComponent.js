30e3d3780d43d25d34c7914bfeab7cf96e509455
josuecedeno <josuecedeno@yahoo.com>
2020-09-04 16:53:07 -0500

    Navigation Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+
+import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
-// import { CAMPSITES } from '../shared/campsites';
 import CampsiteInfo from './CampsiteInfoComponent';
+
 import { View, Platform } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
-import ExpoStatusBar from 'expo-status-bar/build/ExpoStatusBar';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+
+//import ExpoStatusBar from 'expo-status-bar/build/ExpoStatusBar';
 
 const DirectoryNavigator = createStackNavigator(
   {
@@ -25,6 +28,33 @@ const DirectoryNavigator = createStackNavigator(
   }
 );
 
+const HomeNavigator = createStackNavigator(
+  {
+    Home: { screen: Home },
+  },
+  {
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: '#5637DD',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        color: '#fff',
+      },
+    },
+  }
+);
+
+const MainNavigator = createDrawerNavigator(
+  {
+    Home: { screen: HomeNavigator },
+    Directory: { screen: DirectoryNavigator },
+  },
+  {
+    drawerBackgroundColor: '#CEC8FF',
+  }
+);
+
 class Main extends Component {
   render() {
     return (
@@ -35,7 +65,7 @@ class Main extends Component {
             Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight,
         }}
       >
-        <DirectoryNavigator />
+        <MainNavigator />
       </View>
     );
   }
