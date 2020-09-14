e7599145fe3eb3600f5d749fa0c7adb370dd7592
josuecedeno <josuecedeno@yahoo.com>
2020-09-14 18:16:55 -0500

    Drawer Navigator Icons

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,28 +4,21 @@ import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import About from './AboutComponent';
 import Contact from './ContactComponent';
-import { View, Platform, StyleSheet } from 'react-native';
-import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+import {
+  View,
+  Platform,
+  StyleSheet,
+  Text,
+  ScrollView,
+  Image,
+} from 'react-native';
+import {
+  createStackNavigator,
+  createDrawerNavigator,
+  DrawerItems,
+} from 'react-navigation';
 import { Icon } from 'react-native-elements';
-
-//import ExpoStatusBar from 'expo-status-bar/build/ExpoStatusBar';
-
-// const HomeNavigator = createStackNavigator(
-//   {
-//     Home: { screen: Home },
-//   },
-//   {
-//     navigationOptions: {
-//       headerStyle: {
-//         backgroundColor: '#5637DD',
-//       },
-//       headerTintColor: '#fff',
-//       headerTitleStyle: {
-//         color: '#fff',
-//       },
-//     },
-//   }
-// );
+import SafeAreaView from 'react-native-safe-area-view';
 
 const HomeNavigator = createStackNavigator(
   {
@@ -137,10 +130,50 @@ const ContactNavigator = createStackNavigator(
 
 const MainNavigator = createDrawerNavigator(
   {
-    Home: { screen: HomeNavigator },
-    Directory: { screen: DirectoryNavigator },
-    About: { screen: AboutNavigator },
-    Contact: { screen: ContactNavigator },
+    Home: {
+      screen: HomeNavigator,
+      navigationOptions: {
+        drawerIcon: ({ tintColor }) => (
+          <Icon name='home' type='font-awesome' size={24} color={tintColor} />
+        ),
+      },
+    },
+    Directory: {
+      screen: DirectoryNavigator,
+      navigationOptions: {
+        drawerIcon: ({ tintColor }) => (
+          <Icon name='list' type='font-awesome' size={24} color={tintColor} />
+        ),
+      },
+    },
+    About: {
+      screen: AboutNavigator,
+      navigationOptions: {
+        drawerLabel: 'About Us',
+        drawerIcon: ({ tintColor }) => (
+          <Icon
+            name='info-circle'
+            type='font-awesome'
+            size={24}
+            color={tintColor}
+          />
+        ),
+      },
+    },
+    Contact: {
+      screen: ContactNavigator,
+      navigationOptions: {
+        drawerLabel: 'Contact Us',
+        drawerIcon: ({ tintColor }) => (
+          <Icon
+            name='address-card'
+            type='font-awesome'
+            size={24}
+            color={tintColor}
+          />
+        ),
+      },
+    },
   },
   {
     drawerBackgroundColor: '#CEC8FF',
